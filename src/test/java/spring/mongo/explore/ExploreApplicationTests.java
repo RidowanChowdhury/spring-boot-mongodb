2c7dcef0a5022b4c298bb9fb6253fb9da3cb5914
ridwan <Nilesbohr27>
2021-10-18 02:10:58 +0600

    initial commit

---
+++ src/test/java/spring/mongo/explore/ExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mongo.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
